3e204d7147af7f2e495fd66941ce26ff6b6272b1
Talmage <talmagescott123@gmail.com>
2026-01-09 19:17:52 -0500

    Completed Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask for the first and last name of the user and assign them to string variables names "first" and "last" respectively.
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.WriteLine();
+
+        // Writes the name of the person as required by the assignment according to the answers provided by the user earlier.
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
