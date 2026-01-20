4e07dde6992a9596542793cf45d64cd957189899
Talmage <talmagescott123@gmail.com>
2026-01-20 13:49:30 -0500

    Completed LKearning02 Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Creates the resume and assigns the name to it.
+        Resume resume = new Resume();
+        resume._name = "Talmage";
+
+        // Uses the Job class to store information about the first job and store it in the jobs list of the resume.
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        resume._jobs.Add(job1);
+
+        // Uses the job class to store infoirmation abou the second job and store it in the jobs list of the resume.
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        resume._jobs.Add(job2);
+
+        // Displays all of the information.
+        resume.Display();
     }
 }
