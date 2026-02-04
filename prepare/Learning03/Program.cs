fa201dbacc1fad13abf5b6b89cec34d56033d452
Talmage <talmagescott123@gmail.com>
2026-02-03 20:12:09 -0500

    Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac1 = new Fraction();
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetDecimalValue());
+
+        Fraction frac2 = new Fraction(99);
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+
+        Fraction frac3 = new Fraction(8, 9);
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
+
+        Fraction frac4 = new Fraction(-6, 7);
+        Console.WriteLine(frac4.GetFractionString());
+        Console.WriteLine(frac4.GetDecimalValue());
+        
+        Fraction f = new Fraction();
+        Random random = new Random();
+        for (int i = 1; i < 26; i++)
+        {
+            int randomTop = random.Next(50);
+            int randomBottom = random.Next(1, 51);
+            f.SetTop(randomTop);
+            f.SetBottom(randomBottom);
+            Console.WriteLine($"Fraction {i}: string: {f.GetFractionString()} Number: {f.GetDecimalValue()}");
+        }
     }
 }
--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -3,7 +3,6 @@ using System.Security.Cryptography.X509Certificates;
 
 public class Entry
 {
-    public static int _entryNum;
     public string _date;
     public string _prompt;
     public string _entry;
