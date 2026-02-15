d50c713e957e62eb626d22fef6d5ceb7c420087d
Talmage <talmagescott123@gmail.com>
2026-02-14 19:11:38 -0500

    Program.cs comment

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        // To exceed the requirements, I added additional logic to only hide new words
+        // that have not already been hidden. This is done by storing the word and bool
+        // to tell if it should be hidden or not for each Word object. Before the player 
+        // presses enter, it will hide new words but it will check if a word is already
+        // hidden. If a word is already hidden, it will choose a different word at random
+        // until all of the words that need to be hidden are hidden.
+        // Uses the Scripture.HideWords and Word.IsHidden() to do both parts.
+
         string scriptureQuote = "Now behold, a marvelous work is about to come forth among the children of men. Therefore, O ye that embark in the service of God, see that ye serve him with all your heart, might, mind and strength, that ye may stand blameless before God at the last day. Therefore, if ye have desires to serve God ye are called to the work; For behold the field is white already to harvest; and lo, he that thrusteth in his sickle with his might, the same layeth up in store that he perisheth not, but bringeth salvation to his soul; And faith, hope, charity and love, with an eye single to the glory of God, qualify him for the work. Remember faith, virtue, knowledge, temperance, patience, brotherly kindness, godliness, charity, humility, diligence. Ask, and ye shall receive; knock, and it shall be opened unto you. Amen.";
         Reference.CreateReference();
         Scripture scripture = new Scripture(Word.GetWords(scriptureQuote));
@@ -21,7 +29,7 @@ class Program
             wordsLeft = Word.CheckLeftToHide(Scripture.GetWords(scripture)); 
             Scripture.HideWords(scripture, hideAmmount, wordsLeft);
         } while (quit != "quit" ^ wordsLeft == 0);
-        
+
         Console.Clear();
         Console.WriteLine("Goodbye!");
     }
